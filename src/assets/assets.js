08cec8b3a428404e4066dea5a58148a5da8b98e2
caique <caique3066@gmail.com>
2025-05-05 14:26:41 +0200

    firt

---
+++ src/assets/assets.js
@@ -0,0 +1,51 @@
+import logo from './logo.png';
+import cart from './cart.png';
+
+import pasta from './pasta1.jpg';
+import burger from './burger.png';
+import cakes from './cakes.png';
+import icecream from './icecream.png';
+import pizza from './pizza.png';
+import sushi from './sushi.jpeg';
+import salad from './salad.png';
+
+import profile from './profile.png';
+import delivery from './delivery.png';
+
+export const assets = {
+    logo,
+    cart,
+    profile,
+    delivery
+}
+
+export const categories = [
+    {
+        category: 'Pasta',
+        icon: pasta
+    },
+    {
+        category: 'Burger',
+        icon: burger
+    },
+    {
+        category: 'Cake',
+        icon: cakes
+    },
+    {
+        category: 'Ice cream',
+        icon: icecream
+    },
+    {
+        category: 'Pizza',
+        icon: pizza
+    },
+    {
+        category: 'Sushi',
+        icon: sushi
+    },
+    {
+        category: 'Salad',
+        icon: salad
+    }
+];
