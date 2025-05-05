b52a1a3bb52dac132b738f0836aa7cb8ee78eaae
Caiquesantanadias <81467566+Caiquesantanadias@users.noreply.github.com>
2025-05-05 14:33:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: "./", 
+   base: "./", 
   plugins: [react()],
    preview: {
   host: '0.0.0.0',
   port: Number(process.env.PORT) || 4173,
   allowedHosts: true
+}
 })
